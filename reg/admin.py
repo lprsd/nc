45f297c7f161b5fa489277e4adb4b466b1673369
Lakshman Prasad <lakshman@uswaretech.com>
2010-10-01 16:46:52 +0530

    some more changes

---
+++ reg/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from reg.models import Team, Player
+
+admin.site.register(Team)
+admin.site.register(Player)
--- reg/models.py
+++ reg/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=127)
+    
+    
+    
+class Player(models.Model):
+    team = models.ForeignKey()
